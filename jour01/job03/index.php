2ba197fb08e66678e894bf7b0d496f3731b23d3f
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2022-11-14 19:38:51 +0100

    job03 - tableau avec différents types de variables

---
+++ jour01/job03/index.php
@@ -0,0 +1,38 @@
+<?php
+$boolean = "true/false";
+$entier= 20;
+$chaîne_de_caractères = 'bonjour';
+$nombre_à_virguleflottante = 2.95;
+
+echo "<table border=2>
+<thead>
+    <tr>
+      <th> type</th>
+      <th> nom</th>
+      <th> valeur</th>
+    </tr>
+</thead>
+<tbody>
+ <tr>
+    <th>boolean</th>
+    <td>mon_boolean</td>
+    <td>$boolean</td>
+</tr>
+<tr>
+    <th>entier</th>
+    <td>nombre</td>
+    <td>$entier</td>
+</tr>
+<tr>
+    <th>chaîne_de_caractères</th>
+    <td>ma_chaîne_de_caractères</td>
+    <td>$chaîne_de_caractères</td>
+</tr>
+<tr>
+    <th>nombre_à_virgule_flottante</th>
+    <td>mon_nombre_à_virgule_flottante</td>
+    <td>$nombre_à_virguleflottante</td>
+</tr>
+</tbody>
+</table>"
+?>
