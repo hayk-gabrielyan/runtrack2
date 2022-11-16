daf66bdf1fec66133fa6714e4748742e86e13331
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2022-11-16 23:19:41 +0100

    jour03 - job05 - compter et afficher dans un tableau les consonnes et les voyelles dans une chaine des caractères

---
+++ jour03/job05/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$str = "On n’est pas le meilleur quand on le croit mais quand on le sait.";
+echo "$str <br> <br>";
+
+$voyelles = 0;
+$consonnes = 0; 
+
+$array_consonnes = ["b", "c", "d", "f", "g", "h", "j","k", "l", "m", "n", "p","q", "r", "s", "t", "v", "w", "x", "z"];
+$array_voyelles = ["a", "e", "i", "o", "u", "y", "A", "E", "I","O", "U", "Y"];
+
+for ($i = 0; isset($str[$i]); $i++){
+    for($a = 0; isset ($array_voyelles[$a]); $a++){
+        if ($str[$i] == $array_voyelles[$a]){
+            $voyelles++;
+        }
+    }
+    for($b = 0; isset ($array_consonnes[$b]); $b++){
+        if ($str[$i] == $array_consonnes[$a]){
+            $consonnes++;
+        }
+    }
+}
+    $dic = [ "voyelles" => $voyelles ,
+             "consonnes" => $consonnes];
+
+echo "<table border=2>
+    <thead>
+        <tr>
+            <th>Consonnes</th>
+            <th>Voyelles</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>$consonnes</td>
+            <td>$voyelles</td>
+        </tr>
+    </tbody>
+</table>"
+?>
