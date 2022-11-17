9cc0feaaa911f392fef3287a09425da4010882bc
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2022-11-17 11:56:10 +0100

    jour04 - job01 - formulaire HTML + récuperation et affichage des inputs sur une autre page

---
+++ jour04/job01/action.php
@@ -0,0 +1,12 @@
+
+<?php 
+
+echo $_GET['user_name'], '<br>' ; 
+
+echo $_GET['user_mail'] , '<br>' ; 
+
+echo $_GET['user_message'] , '<br>'; 
+
+
+
+?>
---
+++ jour04/job01/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="style.css">
+    <title>My test page</title>
+  </head>
+  <body>
+
+
+<form action="action.php" method="get">
+
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="name" name="user_name" placeholder="saissisez ici votre nom...">
+    </div>
+    <div>
+        <label for="mail">e-mail&nbsp;:</label>
+        <input type="email" id="mail" name="user_mail" placeholder="saissisez ici votre mail...">
+    </div>
+    <div>
+        <label for="msg">Message :</label>
+        <textarea id="msg" name="user_message" placeholder="saissisez ici votre message..."></textarea>
+    </div>
+
+    <div class="button">
+        <button type="submit">Envoyer le message</button>
+    </div>
+</form>
+
+
+
+  </body>
+</html>
