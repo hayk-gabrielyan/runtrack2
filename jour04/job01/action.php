eb3a2a6abcee7e53288e2cdebe39dd52c7b2d9d3
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2022-11-17 13:45:46 +0100

    jour04 - job01 - ajout de la boucle qui compte le nombre d'argumens

--- jour04/job01/action.php
+++ jour04/job01/action.php
@@ -8,5 +8,11 @@ echo $_GET['user_mail'] , '<br>' ;
 echo $_GET['user_message'] , '<br>'; 
 
 
-
+//echo "nombre d'arguments est : " , sizeof($_GET);
+$nombre=0;
+foreach ($_GET as $cle => $valeur) {
+//echo 'La clé ' . $cle . ' contient la valeur ' . $valeur . "\n";
+    $nombre++;
+}
+echo "<p> Nombre des arguments : $nombre <p>";
 ?>
