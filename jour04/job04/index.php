a2b31246c8a971d899b5b695d7067844dd9f5d1c
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2022-11-18 10:39:42 +0100

    jour04 - job04 -  Algorithme qui affiche dans un tableau HTML l’ensemble des arguments  et les valeurs associées.

---
+++ jour04/job04/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="style.css">
+    <title>My test page</title>
+  </head>
+  <body>
+
+
+<form action="" method="post">
+
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" name="Nom" placeholder="saissisez ici votre Nom...">
+    </div>
+    <div>
+        <label for="name">Prénom :</label>
+        <input type="text" name="Prénom" placeholder="saissisez ici votre Prénom...">
+    </div>
+
+    <div class="button">
+        <button type="submit">Envoyer les données</button>
+    </div>
+</form>
+
+<?php
+
+if ($_POST){
+echo '<table border=2>
+<thead>
+  <tr>
+    <th>Argument</th>
+    <th>Valeur</th>
+  </tr>
+</thead>';
+}
+
+foreach($_POST as $argument => $valeur) {
+  echo "<tr>
+    <td>$argument</td>
+      <td>$valeur</td>
+    </tr>";
+}
+
+?>
+
+  </body>
+</html>
