f3c7dd4ff3b9cbdb66a0df54a367bdd54f431496
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2022-11-25 14:58:48 +0100

    jour10 - job10 - affichage des informations des salles en les triant par capacité croissante

---
+++ jour10/job10/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$mysqli = new mysqli("localhost", "root", "", "jour09");
+$mysqli->set_charset("utf8");
+
+$request = $mysqli->query("SELECT * FROM salles ORDER BY capacite ASC ;");
+
+$result = $request -> fetch_array(MYSQLI_ASSOC);
+
+
+    echo "<table border=1> <thead>";
+        foreach ($result as $clé => $valeur)
+        {
+        echo " <th> ". $clé . " </th> ";
+        }
+    echo "</thead>";
+
+
+while($result != NULL){
+    echo "<tbody>";
+        foreach ($result as $valeur)
+        {
+        echo  "<td>" . $valeur ."</td>"   ;
+        }
+    echo "</tbody>";
+    $result = $request -> fetch_array(MYSQLI_ASSOC);
+}
+echo "</table>";
+
+?>
+</body>
+</html>
